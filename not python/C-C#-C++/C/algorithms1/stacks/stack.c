7d79b0ac0aca9c6ec7af4e7d041b86afed59b383
Bruno Mendes <bruno.mendes.003@gmail.com>
2024-06-26 14:28:41 -0300

    aslkdjansdk

---
+++ not python/C-C#-C++/C/algorithms1/stacks/stack.c
@@ -0,0 +1,9 @@
+#include "stack.h"
+
+void cpush(char y){
+
+}
+
+char cpop(void){
+	
+}
---
+++ not python/C-C#-C++/C/bintrees/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	char info;
+	struct node *l, *r;
+};
+
+int main(int argc, char const *argv[])
+{
+	
+
+	return 0;
+}
