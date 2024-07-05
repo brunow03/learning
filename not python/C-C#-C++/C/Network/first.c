84dc90b0c15abf61750473f46ee39fa97432de67
Bruno Mendes <bruno.mendes.003@gmail.com>
2024-07-04 23:43:59 -0300

    a,sdnalsdkasd

--- not python/C-C#-C++/C/Network/first.c
+++ not python/C-C#-C++/C/Network/first.c
@@ -1,51 +1,67 @@
-#include <stdio.h>
-#include <sys/types.h>
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Lewis Van Winkle
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string>
 
-void error(const char *msg)
-{
-	perror(msg);
-	exit(1);
-}
 
-int main(int argc, char const *argv[])
-{
-	if (argc < 2)
-	{
-		fprintf(stderr, "Port No not provided. Program terminated.\n");
-		exit(1);
-	}
+int main() {
 
-	int sockfd, newsockfd, portno, n;
-	char buffer[255];
+    struct ifaddrs *addresses;
 
-	struct sockaddr_in serv_addr, cli_addr;
-	socklen_t clilen;
+    if (getifaddrs(&addresses) == -1) {
+        printf("getifaddrs call failed\n");
+        return -1;
+    }
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
-	{
-		error("Error opening Socket.");
-	}
+    struct ifaddrs *address = addresses;
+    while(address) {
+        if (address->ifa_addr == NULL) { 
+            address = address->ifa_next;
+            continue;
+        }
+        int family = address->ifa_addr->sa_family;
+        if (family == AF_INET || family == AF_INET6) {
 
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	portno = atoi(argv[1]);
+            printf("%s\t", address->ifa_name);
+            printf("%s\t", family == AF_INET ? "IPv4" : "IPv6");
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
+            char ap[100];
+            const int family_size = family == AF_INET ?
+                sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6);
+            getnameinfo(address->ifa_addr,
+                    family_size, ap, sizeof(ap), 0, 0, NI_NUMERICHOST);
+            printf("\t%s\n", ap);
 
-	if (bind(sockfd), ((struct sockaddr *) &serv_addr, sizeof(serv_addr) < 0))
-		error("Binding failed.");
+        }
+        address = address->ifa_next;
+    }
 
-	listen(sockfd, 5);
-	clilen = sizeof(cli_addr);
 
-	newsockfd = accept();
-
-	return 0;
+    freeifaddrs(addresses);
+    return 0;
 }
