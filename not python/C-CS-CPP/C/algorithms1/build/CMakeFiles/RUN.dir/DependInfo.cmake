e7e7a0a3e5634076b8c61b76bab821697f5dc57c
Bruno Mendes <bruno.mendes.003@gmail.com>
2024-07-27 14:26:19 -0300

    something

---
+++ not python/C-CS-CPP/C/algorithms1/build/CMakeFiles/RUN.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/aside.c" "CMakeFiles/RUN.dir/src/aside.c.o" "gcc" "CMakeFiles/RUN.dir/src/aside.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/binarytrees.c" "CMakeFiles/RUN.dir/src/binarytrees.c.o" "gcc" "CMakeFiles/RUN.dir/src/binarytrees.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/general_sort.c" "CMakeFiles/RUN.dir/src/general_sort.c.o" "gcc" "CMakeFiles/RUN.dir/src/general_sort.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/hashing.c" "CMakeFiles/RUN.dir/src/hashing.c.o" "gcc" "CMakeFiles/RUN.dir/src/hashing.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/heapsort.c" "CMakeFiles/RUN.dir/src/heapsort.c.o" "gcc" "CMakeFiles/RUN.dir/src/heapsort.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/linked_list.c" "CMakeFiles/RUN.dir/src/linked_list.c.o" "gcc" "CMakeFiles/RUN.dir/src/linked_list.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/main.c" "CMakeFiles/RUN.dir/src/main.c.o" "gcc" "CMakeFiles/RUN.dir/src/main.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/mergesort.c" "CMakeFiles/RUN.dir/src/mergesort.c.o" "gcc" "CMakeFiles/RUN.dir/src/mergesort.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/queue.c" "CMakeFiles/RUN.dir/src/queue.c.o" "gcc" "CMakeFiles/RUN.dir/src/queue.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/quicksort.c" "CMakeFiles/RUN.dir/src/quicksort.c.o" "gcc" "CMakeFiles/RUN.dir/src/quicksort.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/radixsort.c" "CMakeFiles/RUN.dir/src/radixsort.c.o" "gcc" "CMakeFiles/RUN.dir/src/radixsort.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/randarray.c" "CMakeFiles/RUN.dir/src/randarray.c.o" "gcc" "CMakeFiles/RUN.dir/src/randarray.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/stacks.c" "CMakeFiles/RUN.dir/src/stacks.c.o" "gcc" "CMakeFiles/RUN.dir/src/stacks.c.o.d"
+  "/home/bruno/Git/learning/not python/C-CS-CPP/C/algorithms1/src/stringmatching.c" "CMakeFiles/RUN.dir/src/stringmatching.c.o" "gcc" "CMakeFiles/RUN.dir/src/stringmatching.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
