792ccca3333d339bba8932c08cb3c429bcb24e58
brunow03 <bruno.mendes.003@gmail.com>
2025-12-08 13:34:20 -0300

    commit

--- cpp/sfml/tutorial/tutorial_10/src/main.cpp
+++ cpp/sfml/tutorial/tutorial_10/src/main.cpp
@@ -3,22 +3,12 @@
 #include "SFML/Window.hpp"
 #include "SFML/System.hpp"
 
-#include <vector>
-
 using namespace sf;
-// using namespace std;
 
 int main(int argc, char const *argv[])
 {
     RenderWindow window(VideoMode(640,400), "Example 1", Style::None);
     window.setFramerateLimit(60);
-
-    // Balls
-    CircleShape player;
-    std::vector<CircleShape> projectiles;
-    
-    std::vector<RectangleShape> enemies;
-
 
     while (window.isOpen())
     {
